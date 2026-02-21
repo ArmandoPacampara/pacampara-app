a62103b25dc4abeff26b621b027a1c02a9cbf33a
Armando <mandopacampara@gmail.com>
2026-02-21 17:44:35 +0800

    Exercise #3 - Routing, Middleware, and CSRF

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use App\Services\UserService;
 use Illuminate\Support\Facades\Response;
 
 
 Route::get('/', function () {
     return view('welcome');
+    return 'hello world!';
 });
 
-Route::get('show-users', [\App\Http\Controllers\UserController::class, 'show']);
+Route::get('show-users', [UserController::class, 'show']);
 
 //service container
 Route::get('/test-container', function (Request $request) {
@@ -30,4 +31,52 @@ Route::get('/test-users', [UserController::class, 'index']);
 // Facades
 Route::get('test-facade', function (UserService $userService) {
     return Response::json($userService->listUsers());
+});
+
+
+// Exercise #3
+
+//Routing -> Parameters
+Route::get('/post/{post}/comment/{comment}', function (string $postId, string $comment) {
+    return "Post ID: $postId, Comment ID: $comment";
+});
+
+Route::get ('/post/{id}', function (string $id){
+    return $id;
+})-> where('id', '[0-9]+');
+
+Route::get('/search/{search}', function (string $search) {
+    return $search;
+})-> where('search', '.*');
+
+//Named Route or Route Alias
+Route::get('test/route/sample', function(){
+    return route('test-route');
+})->name('test-route');
+
+//Route -> Middleware Group 
+Route::middleware(['user-middleware'])->group(function(){
+    Route::get('route-middleware-group/first', function(Request $request){
+        echo 'first';
+    });
+
+    Route::get('route-middleware-group/second', function(Request $request){
+        echo 'second';
+    });
+});
+
+//Route -> Controller Group 
+Route::controller(UserController::class)->group(function(){
+    Route::get('/users','index');
+    Route::get('/users/first','first');
+    Route::get('/users/{id}','get');
+});
+
+//CSRF
+Route::get('/token', function (Request $request) {
+    return view('token');
+});
+
+Route::post('/token', function (Request $request) {
+    return $request->all();
 });
